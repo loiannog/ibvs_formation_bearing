6804922323046c0734726923c3aa931ce0951a32
Joe Polin <jpolin@seas.upenn.edu>
2013-12-29 23:53:42 -0500

    Removed non-pertinent files.

--- devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/giuseppe/catkinAIRobotsUnina_ws/src/AIRobots_Unina_workspace/ibvs_formation_bearing/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
