581ac0a6ddd46419f459331f0c8fbb12778ca561
Joe Polin <jpolin@seas.upenn.edu>
2013-12-31 12:29:10 -0500

    Added files so that I can checkout.

---
+++ CMakeFiles/circle_detection.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jpolin/Documents/gitHub/ROS_workspace/ibvs_formation_bearing/src/get_circle.cpp" "/home/jpolin/Documents/gitHub/ROS_workspace/ibvs_formation_bearing/CMakeFiles/circle_detection.dir/src/get_circle.cpp.o"
+  "/home/jpolin/Documents/gitHub/ROS_workspace/ibvs_formation_bearing/src/imgproc.cpp" "/home/jpolin/Documents/gitHub/ROS_workspace/ibvs_formation_bearing/CMakeFiles/circle_detection.dir/src/imgproc.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jpolin/Documents/gitHub/ROS_workspace/ibvs_formation_bearing/CMakeFiles/lib.dir/DependInfo.cmake"
+  )
---
+++ CMakeFiles/lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lib.dir/src/imgproc.cpp.o"
+  "lib/liblib.pdb"
+  "lib/liblib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
