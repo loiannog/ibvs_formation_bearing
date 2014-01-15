0b988a8ef6cd1128684d2b5a2522162a7e36188c
loiannog <loiannog@gmail.com>
2014-01-15 21:48:11 +0100

    fixing colors

---
+++ CMakeFiles/ROSBUILD_genmsg_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "msg_gen"
+  "msg_gen"
+  "src/ibvs_formation_bearing/msg"
+  "CMakeFiles/ROSBUILD_genmsg_cpp"
+  "msg_gen/cpp/include/ibvs_formation_bearing/bearing.h"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/giuseppe/git/ibvs_formation_bearing/test_results"
+export ROS_TEST_RESULTS_DIR="/home/giuseppe/git/ibvs_formation_bearing/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/giuseppe/git/ibvs_formation_bearing/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/giuseppe/git/ibvs_formation_bearing/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/giuseppe/git/ibvs_formation_bearing/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/giuseppe/git/ibvs_formation_bearing/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/giuseppe/git/ibvs_formation_bearing/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/giuseppe/git/ibvs_formation_bearing/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/giuseppe/git/ibvs_formation_bearing/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/giuseppe/git/ibvs_formation_bearing:/opt/ros/hydro/share:/opt/ros/hydro/stacks"
