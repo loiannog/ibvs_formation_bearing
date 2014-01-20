e48b8190bdfbc424c83c49fab5fac1f80469fded
loiannog <loiannog@gmail.com>
2014-01-20 03:30:16 +0100

    publis numbe rof ellipses

--- src/get_circle.cpp
+++ src/get_circle.cpp
@@ -119,6 +119,7 @@ void getCircle::camera_callback(const sensor_msgs::Image::ConstPtr &img)
     ellipses.bearings.push_back(ellipse_direction2);
     ellipses.color[1] = color2;
     }
+    cout<<"number_of_ellipses:"<<ellipses.bearings.size()<<endl;
     if(ellipses.bearings.size() > 0)
     ellipse_pos_pub_.publish(ellipses);
 
